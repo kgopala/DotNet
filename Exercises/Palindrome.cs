e8e11e6c8cdc2538beb20cf3a3e6393b3e4dfd7b
Gopala Krishna <krishna.gopala@chamberlain.com>
2019-06-07 14:49:02 -0500

    Committing palindrome class

---
+++ Exercises/Palindrome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects.Exercises
+{
+    class Palindrome
+    {
+        public string palindrome(string InputString)
+        {
+            Console.WriteLine("Enter a String: ");
+            String takeAString = Console.ReadLine();
+            return InputString;
+        }
+    }
+}
